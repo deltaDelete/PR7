bdee788704e7c32965f3a7f4734be29a2bfb4f53
deltaDelete <samansy@yandex.ru>
2023-10-27 13:31:48 +0700

    WIP

--- App/ViewModels/MainWindowViewModel.cs
+++ App/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using App.Models;
 using Microsoft.EntityFrameworkCore;
@@ -8,28 +10,10 @@ using ReactiveUI;
 
 namespace App.ViewModels;
 
-public class MainWindowViewModel : ViewModelBase
-{
+public class MainWindowViewModel : ViewModelBase {
     public ApplicationDbContext Database { get; private set; }
 
     public MainWindowViewModel() {
-        this.WhenAnyValue(x => x.Database)
-            .Subscribe(EnsureDatabase);
-    }
-
-    private async void EnsureDatabase(ApplicationDbContext? db) {
-        if (db is null || db.Patients.Any()) {
-            Database = new ApplicationDbContext();
-            await Database.Database.OpenConnectionAsync();
-            return;
-        }
-
-        await db.Database.EnsureCreatedAsync();
-        await db.Patients.AddAsync(new Patient() {
-            Address = "Аскабан",
-            Email = "user@example.com",
-            FullName = "Сигмов Сигма Сигмович",
-            Phone = "+7 912 345 67 89"
-        });
+        // TODO: Адаптировать код из прошлых работ
     }
 }
